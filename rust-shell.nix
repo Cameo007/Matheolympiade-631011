f410e7a96fd5f0e7a7e37389c3b2ed93f3e9bf04
Pascal Dietrich <pascal.1.dietrich@hotmail.com>
2023-10-04 21:50:38 +0200

    modified rust shell

--- rust-shell.nix
+++ rust-shell.nix
@@ -1,7 +1,3 @@
-/*
-based on
-https://discourse.nixos.org/t/how-can-i-set-up-my-rust-programming-environment/4501/9
-*/
 let
   rust_overlay = import (builtins.fetchTarball "https://github.com/oxalica/rust-overlay/archive/master.tar.gz");
   pkgs = import <nixpkgs> { overlays = [ rust_overlay ]; };
