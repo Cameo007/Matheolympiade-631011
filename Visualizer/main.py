63e3ee544bfbf85282e1eccbba857621bb8d523b
Pascal Dietrich <80521473+Cameo007@users.noreply.github.com>
2023-10-05 21:16:21 +0200

    Update main.py

--- Visualizer/main.py
+++ Visualizer/main.py
@@ -1,14 +1,21 @@
-import matplotlib.pyplot as plt
-import numpy as np
+import sys
 import json
+import numpy as np
+import matplotlib.pyplot as plt
 
-with open("result_count.txt", "r") as file:
-	points = json.load(file)
-	points = {int(key):int(value) for key,value in points.items()}
-	xpoints = np.array(list(points.keys()))
-	ypoints = np.array(list(points.values()))
-
-	plt.xlabel("s")
-	plt.ylabel("Anzahl der möglichen Quersummen")
-	plt.plot(xpoints, ypoints, "o")
-	plt.savefig("result_count.png")
+if len(sys.argv) == 1:
+	try:
+		with open(sys.argv[1], "r") as file:
+			points = json.load(file)
+			points = {int(key):int(value) for key,value in points.items()}
+			xpoints = np.array(list(points.keys()))
+			ypoints = np.array(list(points.values()))
+		
+			plt.xlabel("s")
+			plt.ylabel("Anzahl der möglichen Quersummen")
+			plt.plot(xpoints, ypoints, "o")
+			plt.savefig("result_count.png")
+	except FileNotFoundError:
+		print(f"Die Datei {sys.argv[1]} existiert nicht.")
+else:
+	print(f"Benutzung: {sys.argv[0]} <file>")
