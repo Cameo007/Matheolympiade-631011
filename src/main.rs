9d2039e27960afe0004c1bde06e08648d4434982
Pascal Dietrich <pascal.1.dietrich@hotmail.com>
2023-10-09 07:55:22 +0200

    Plots can now generated by the Rust application.

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,10 @@ use num_bigint::{BigUint, ToBigUint};
 use std::collections::HashMap;
 use std::fs;
 use std::fs::OpenOptions;
+use plotlib::page::Page;
+use plotlib::repr::Plot;
+use plotlib::view::ContinuousView;
+use plotlib::style::PointStyle;
 
 fn main() {
     let mut args: Vec<String> = env::args().collect();
@@ -36,8 +40,23 @@ fn main() {
     }
 }
 
+
+fn plot(data: Vec<(f64, f64)>, s_start: usize, s_stop: usize) {
+    let plot: Plot = Plot::new(data).point_style(
+        PointStyle::new()
+            .colour("#35C788"),
+    );
+
+    let view = ContinuousView::new()
+        .add(plot)
+        .x_label("s")
+        .y_label("Anzahl der möglichen Quersummen");
+
+    Page::single(&view).save(format!("./Data/result_count_s{}-{}.svg", s_start, s_stop)).unwrap();
+}
+
 fn count_cross_sums_per_s(start: usize, stop: usize) {
-	let mut cross_sums_per_s: HashMap<String, usize> = HashMap::new();
+	let mut cross_sums_per_s: HashMap<u64, u64> = HashMap::new();
 
     for s in start..stop {
         let k_vec: Vec<u8>  = vec![1; s];
@@ -68,10 +87,20 @@ fn count_cross_sums_per_s(start: usize, stop: usize) {
             println!("");
         }
 
-        cross_sums_per_s.insert(s.to_string(), cross_sums.len());
+        cross_sums_per_s.insert(s as u64, cross_sums.len() as u64);
     }
 
-    fs::write(format!("./Data/result_count_s{}-{}.json", start, stop), &serde_json::to_string(&cross_sums_per_s).unwrap()).expect("Unable to write file");
+    let cross_sums_per_s_json: HashMap<String, u64> = HashMap::from_iter(
+        cross_sums_per_s.clone().into_iter()
+        .map(| (key, value)| (key.to_string(), value))
+    );
+    fs::write(format!("./Data/result_count_s{}-{}.json", start, stop), &serde_json::to_string(&cross_sums_per_s_json).unwrap()).expect("Unable to write file");
+
+    let plot_data = Vec::from_iter(cross_sums_per_s
+        .into_iter()
+        .map(|(key, value)| ((key as f64) , (value as f64)))
+    );
+    plot(plot_data, start, stop);
 }
 
 fn find_cross_sums_per_s(start: usize, stop: usize) {
